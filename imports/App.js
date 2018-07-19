9dce1c70982471a404d63807a6e22ee8100c29e8
Semih Ucari <semih.ucari@icterra.com>
2018-07-19 15:46:25 +0300

    before sizing

--- imports/App.js
+++ imports/App.js
@@ -9,21 +9,37 @@ export default class App extends Component {
                 <Table celled>
                     <Table.Body>
                         <Table.Row>
-                            <Table.Cell></Table.Cell>
+                            <Table.Cell textAlign="center">
+                                <Icon name="close" size="massive"/>
+                            </Table.Cell>
+                            <Table.Cell active textAlign="center">
+                                <Icon name="circle outline" size="massive"/>
+                            </Table.Cell>
+                            <Table.Cell textAlign="center">
+                                <Icon name="close" size="massive"/>
+                            </Table.Cell>
+                        </Table.Row>
+                        <Table.Row>
+                            <Table.Cell active textAlign="center">
+                                <Icon name="circle outline" size="massive"/>
+                            </Table.Cell>
+                            <Table.Cell textAlign="center">
+                                <Icon name="close" size="massive"/>
+                            </Table.Cell>
                             <Table.Cell active textAlign="center">
                                 <Icon name="close" size="massive"/>
                             </Table.Cell>
-                            <Table.Cell></Table.Cell>
                         </Table.Row>
                         <Table.Row>
-                            <Table.Cell active></Table.Cell>
-                            <Table.Cell></Table.Cell>
-                            <Table.Cell active></Table.Cell>
-                        </Table.Row>
-                        <Table.Row>
-                            <Table.Cell></Table.Cell>
-                            <Table.Cell active></Table.Cell>
-                            <Table.Cell></Table.Cell>
+                            <Table.Cell textAlign="center">
+                                <Icon name="circle outline" size="massive"/>
+                            </Table.Cell>
+                            <Table.Cell active textAlign="center">
+                                <Icon name="close" size="massive"/>
+                            </Table.Cell>
+                            <Table.Cell textAlign="center">
+                                <Icon name="close" size="massive"/>
+                            </Table.Cell>
                         </Table.Row>
                     </Table.Body>
                 </Table>
